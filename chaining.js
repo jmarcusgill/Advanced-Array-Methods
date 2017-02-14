c01d2831b12ad186193653c3ae8e2d5b2512ab69
jmarcusgill <jmarcusgill@gmail.com>
2017-02-14 17:51:46 -0600

    everything is done on one line..looks ugly but it fits the directions

--- chaining.js
+++ chaining.js
@@ -6,20 +6,60 @@ var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 // 1.  Sort the numbers in descending order (10, 9, 8, 7, etc).
 integers.sort(function(a,b) {return b-a});
-console.log("reverse sorted", integers);
+// console.log("reverse sorted", integers);
 
 
 // 2.  Remove any integers greater than 19.
 var chaingingResult = integers.sort(function(a,b){return b-a}).filter(function(num){return num <19});
-console.log("nothing over 19", chaingingResult);
+// console.log("nothing over 19", chaingingResult);
 
 // 3.  Multiply each remaining number by 1.5 and then subtract 1.
 var newTotal = chaingingResult.map(function(x) {
   return (x * 1.5) -1
 })
-console.log("new total", newTotal);
+// console.log("new total", newTotal);
 
 // 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
 var newSum = newTotal.reduce(function (j, k){return j+k})
-console.log("new total", newSum)
+// console.log("new total", newSum)
+
+
+
+// Using one single line of JavaScript code, complete the following tasks on the array of integers below.
+
+// 1.  Sort the numbers in descending order (10, 9, 8, 7, etc).
+// 2.  Remove any integers greater than 19.
+// 3.  Multiply each remaining number by 1.5 and then subtract 1.
+// 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
+
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+// All of the strings on one line...
+var chainingResult = integers.sort(function(a,b){return b-a}).filter(function(num){return num < 19}).map(function(x){return (x * 1.5) - 1}).reduce(function (a,b){return a + b});
+console.log("chainingResult: ", chainingResult)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
