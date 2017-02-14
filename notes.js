9a25c7fd750e9bc1af39098db842d8cb0321d892
jmarcusgill <jmarcusgill@gmail.com>
2017-02-13 20:09:46 -0600

    class notes

--- notes.js
+++ notes.js
@@ -1,92 +1,92 @@
-var fruits = ["apple", "banana", "cherry"]
+// var fruits = ["apple", "banana", "cherry"]
 
-//1A: Write a function called outputFruitOld that consoles every fruit in a fruit array
+// //1A: Write a function called outputFruitOld that consoles every fruit in a fruit array
 
-function outputFruitOld (array) {
-  for (var i = 0; i < array.length; i++) {
-    // console.log(array[i]);
-  }
-}
-
-outputFruitOld(fruits);
-
-//1B: Rewrite using forEach array method
-
-//// function outputFruits (element, index, array) {
-////   console.log(element);
-////   console.log("array[" + index + "] " + element);
-//// }
-
-//// fruits.forEach(outputFruits)
-
-//// fruits.forEach(function(element) {
-////   console.log(element)
-//// })
-
-//2A: Write a function called backwardsFruitsOld that reverses the letters in each word in the fruits array.
-
-// function backwardsFruitsOld(array) {
-//   var backwardsFruit = [];
-//   for (var i = 0; i < array.length; i++){
-//     backwardsFruit.push(array[i].split("").reverse().join(""));
+// function outputFruitOld (array) {
+//   for (var i = 0; i < array.length; i++) {
+//     // console.log(array[i]);
 //   }
-//   console.log(backwardsFruit);
 // }
 
-// backwardsFruitsOld(fruits)
+// outputFruitOld(fruits);
 
-// //2B: Rewrite using map array method
+// //1B: Rewrite using forEach array method
 
-// var backwardsFruit = fruits.map(function(fruits){
-//   return fruits.split("").reverse().join("");
+// //// function outputFruits (element, index, array) {
+// ////   console.log(element);
+// ////   console.log("array[" + index + "] " + element);
+// //// }
+
+// //// fruits.forEach(outputFruits)
+
+// //// fruits.forEach(function(element) {
+// ////   console.log(element)
+// //// })
+
+// //2A: Write a function called backwardsFruitsOld that reverses the letters in each word in the fruits array.
+
+// // function backwardsFruitsOld(array) {
+// //   var backwardsFruit = [];
+// //   for (var i = 0; i < array.length; i++){
+// //     backwardsFruit.push(array[i].split("").reverse().join(""));
+// //   }
+// //   console.log(backwardsFruit);
+// // }
+
+// // backwardsFruitsOld(fruits)
+
+// // //2B: Rewrite using map array method
+
+// // var backwardsFruit = fruits.map(function(fruits){
+// //   return fruits.split("").reverse().join("");
+// // });
+// // console.log(backwardsFruit)
+
+// //3A: write a function called fiveLettersOnlyOld that only returns fruits that have 5 letters
+
+// function fiveLettersOnlyOld (array) {
+//   var filteredFruits = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i].length === 5) {
+//       filteredFruits.push(array[i]);
+//     }
+//   }
+//   // console.log(filteredFruits)
+// }
+// fiveLettersOnlyOld(fruits)
+
+// //3B: Rewrite using filter array method
+
+// function filterIt (cat) {
+//   if (cat.length === 5) {
+//     return cat;
+//   }
+// }
+//  var filteredFruits = fruits.filter(filterIt);
+//  console.log(filteredFruits);
+
+// //4A: Write a function called addzOld that returns the sum of an array.
+
+// var numbers = [0, 1, 2, 3, 4];
+
+// function addzOld(array){
+//   var sum = 0;
+//   for (var i = 0; i < array.length; i++){
+//     sum = sum + array[i];
+//   }
+//   console.log(sum);
+// }
+// addzOld(numbers);
+
+// //4B: Rewrite using reduce method
+
+// var sum = [0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, index) {
+//   console.log("current index " + index);
+//   console.log("previousValue ", previousValue);
+//   console.log("currentValue", currentValue)
+//   return previousValue + currentValue;
 // });
-// console.log(backwardsFruit)
-
-//3A: write a function called fiveLettersOnlyOld that only returns fruits that have 5 letters
-
-function fiveLettersOnlyOld (array) {
-  var filteredFruits = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i].length === 5) {
-      filteredFruits.push(array[i]);
-    }
-  }
-  // console.log(filteredFruits)
-}
-fiveLettersOnlyOld(fruits)
-
-//3B: Rewrite using filter array method
-
-function filterIt (cat) {
-  if (cat.length === 5) {
-    return cat;
-  }
-}
- var filteredFruits = fruits.filter(filterIt);
- console.log(filteredFruits);
-
-//4A: Write a function called addzOld that returns the sum of an array.
-
-var numbers = [0, 1, 2, 3, 4];
-
-function addzOld(array){
-  var sum = 0;
-  for (var i = 0; i < array.length; i++){
-    sum = sum + array[i];
-  }
-  console.log(sum);
-}
-addzOld(numbers);
-
-//4B: Rewrite using reduce method
-
-var sum = [0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, index) {
-  console.log("current index " + index);
-  console.log("previousValue ", previousValue);
-  console.log("currentValue", currentValue)
-  return previousValue + currentValue;
-});
-console.log(sum);
+// console.log(sum);
 
 
 
